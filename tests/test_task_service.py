6b387322c8f12d1a0e28fdd94f0e8179877bfec8
Mathvicente <134223943+Mathvicente@users.noreply.github.com>
2026-01-03 00:11:02 -0300

    Update test_task_service.py

--- tests/test_task_service.py
+++ tests/test_task_service.py
@@ -1,31 +1,19 @@
-from task_service import *
+import src.task_service as service
 
 def setup_function():
-    service.tasks.clear()
-    service.counter = 1
-
+    service.reset_tasks()
 
 def test_criar_tarefa_com_prioridade():
     tarefa = service.criar_tarefa("Estudar", "Revisar matéria", prioridade="Alta")
     assert tarefa["prioridade"] == "Alta"
-    assert tarefa["id"] == 1
-
 
 def test_listar_tarefas():
-    service.criar_tarefa("Tarefa A", "Desc")
+    service.criar_tarefa("Exemplo", "Teste")
     tarefas = service.listar_tarefas()
     assert len(tarefas) == 1
 
-
 def test_atualizar_prioridade():
-    service.criar_tarefa("Tarefa B", "Desc")
+    service.criar_tarefa("A", "B")
     service.atualizar_tarefa(1, prioridade="Baixa")
     tarefas = service.listar_tarefas()
     assert tarefas[0]["prioridade"] == "Baixa"
-
-
-def test_remover_tarefa():
-    service.criar_tarefa("Tarefa C", "Desc")
-    service.remover_tarefa(1)
-    tarefas = service.listar_tarefas()
-    assert len(tarefas) == 0
